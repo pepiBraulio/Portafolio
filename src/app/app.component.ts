8384c666719e25141eb734367dfb008f699f1c62
brauliogm <brauliogm16@gmail.com>
2023-12-01 20:25:39 -0600

    Acabamos el header y las primeras partes de articulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class AppComponent {
   title = 'portafolio';
 
-  imagenURL: SafeResourceUrl;
+  logoPrincipal: SafeResourceUrl;
+  ilustracionIntro: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
     // Ruta relativa a la imagen en la carpeta assets
     const rutaImagen = 'assets/logo.svg';
+    const ilustracionIntro1 = 'assets/illustration-intro.svg';
 
     // Sanitizar la URL de la imagen para evitar problemas de seguridad
-    this.imagenURL = this.sanitizer.bypassSecurityTrustResourceUrl(rutaImagen);
+    this.logoPrincipal = this.sanitizer.bypassSecurityTrustResourceUrl(rutaImagen);
+    this.ilustracionIntro = this.sanitizer.bypassSecurityTrustResourceUrl(ilustracionIntro1);
   }
 }
